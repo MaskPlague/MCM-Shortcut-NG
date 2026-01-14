51a0436353a95cf53110ec34110f4ce31a236efa
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-01-14 05:02:22 -0600

    add timer for testing

--- src/pch.h
+++ src/pch.h
@@ -12,3 +12,22 @@ using json = nlohmann::json;
 #include "MCM.h"
 #include "Events.h"
 using namespace std::literals;
+
+/*struct ScopedTimer
+{
+    std::string_view name;
+    std::chrono::high_resolution_clock::time_point start;
+
+    ScopedTimer(std::string_view timerName) : name(timerName)
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    ~ScopedTimer()
+    {
+        auto end = std::chrono::high_resolution_clock::now();
+        // Calculate in microseconds (1/1,000,000th of a second)
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        logger::debug("[BENCHMARK] {} took: {} microseconds", name, duration);
+    }
+};*/
